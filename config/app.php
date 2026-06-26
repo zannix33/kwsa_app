9462487fdf35c9f9c5719389210058833620de00
zannix33 <50485000+zannix33@users.noreply.github.com>
2026-06-26 17:37:36 +0800

    datatable test fix

--- config/app.php
+++ config/app.php
@@ -232,6 +232,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
