cf8cceda40cc8403320d60a09988f13725dfc626
zannix33 <50485000+zannix33@users.noreply.github.com>
2026-07-15 15:04:04 +0800

    Changes in arms

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(EmployeeSeeder::class);
-        $this->call(CompanySeeder::class);
+        //$this->call(EmployeeSeeder::class);
+        //$this->call(CompanySeeder::class);
     }
 }
