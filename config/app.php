2e4252ffb924127c3086683d444b301e104638a5
zannix33 <50485000+zannix33@users.noreply.github.com>
2026-07-15 15:52:55 +0800

    Changes in arms

--- config/app.php
+++ config/app.php
@@ -168,7 +168,7 @@ return [
          * Package Service Providers...
          */
 
-        Yajra\DataTables\DataTablesServiceProvider::class,
+        //Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
